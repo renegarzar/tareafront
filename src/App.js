5e295cfdf81aba20330e60bdb60757dddcc15390
Rene <renaijgr@outlook.com>
2024-09-25 19:12:33 -0600

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import Header from './components/header';
+import Footer from './components/Footer';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [showMessages, setShowMessages] = useState(false);
+  const [message,  setMessage] = useState('');
+  const [message2,  setMessage2] = useState('');
+  
+  const handleImageClick = async () => {
+    setShowMessages(true);
+
+    try {
+      const response = await fetch('http://localhost:3001');
+      const text = await response.text();
+      setMessage(text);
+    } catch (error) {
+      console.error('Error al obtener el mensaje:', error);
+    }
+
+    try {
+      const response = await fetch('http://localhost:3001/mensaje2');
+      const text = await response.text();
+      setMessage2(text);
+    } catch (error) {
+      console.error('Error al obtener el mensaje:', error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <main>
+        <section id="home">
+          <h2>¿Quién es Moo Deng?</h2>
+          <p>Es importante conocer al hipopótamo pigmeo.</p>
+        </section>
+
+        
+        <div className="image-and-messages">
+
+          {showMessages && <p className="message-left">{message}</p>}
+            <nav className="imagen-centrada">
+              <ul>
+               <li>
+                <a href="#imagen" onClick={handleImageClick}>
+                  <img src="/images/moodengpic.jpg" alt="Imagen de Moo Deng"/>
+                </a>
+                </li>
+              </ul>
+            </nav>
+
+          {showMessages && <p className="message-right">{message2}</p>}
+
+        </div>
+
+
+        <section id="info">
+          <h2>¿Quieres saber más?</h2>
+          <p>Haz click en la imagen.</p>
+        </section>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+        <h1>Moo Deng</h1>
+        </header>
+
+    );
+}
+
+export default Header;
